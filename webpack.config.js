552db62d2c19091b98cd1da381e7522ac3cde47d
Konstantin <konstantinz@wizardsdev.com>
2026-08-10 00:20:37 +0300

    Add cache busting for production bundle

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     publicPath: '/coach-card/',
     clean: true,
   },
