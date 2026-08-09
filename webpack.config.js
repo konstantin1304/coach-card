a54457de14598d4577d043392bcd9597896e364a
Konstantin <konstantinz@wizardsdev.com>
2026-08-10 00:06:45 +0300

    Fix production JSX build

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,13 @@ module.exports = {
           options: {
             presets: [
               '@babel/preset-env',
-              ['@babel/preset-react', { runtime: 'automatic' }],
+              [
+                '@babel/preset-react',
+                {
+                  runtime: 'automatic',
+                  development: false,
+                },
+              ],
             ],
           },
         },
